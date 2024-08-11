4b247ba8038bc24973c5c7bcd5bf1379580472fa
Omar Barra <78673626+Baraski-jr@users.noreply.github.com>
2024-08-11 18:02:34 +0100

    Improve the responsive of the subHero section for the mobile view

--- components/featureProducts/index.tsx
+++ components/featureProducts/index.tsx
@@ -14,23 +14,59 @@ import ProductCart from '../ui/cart';
 
 const FeatureProducts = () => {
     return (
-        <section className=" container ">
+        <section className="container">
           <header className="text-center space-y-2 py-3">
-            <h1 className="font-bold text-slate-700 text-2xl"> FEATURED PRODUCTS </h1>
-            <p className="text-slate-500">The most prominent product in the store, which was bought with the highest number </p>
-          </header>
-          <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 2xl:grid-cols-5 gap-5 py-4">
-              <ProductCart name={'air'} url={'/products/a1.png'} price={300} />
-              <ProductCart name={'nike'} url={'/products/b1.png'} price={200} />
-              <ProductCart name={'adidas'} url={'/products/c1.png'} price={2500} />
-              <ProductCart name={'pumer'} url={'/products/d1.png'} price={500} />
-              <ProductCart name={'air'} url={'/products/e1.png'} price={700} />
-              <ProductCart name={'nike'} url={'/products/f1.png'} price={1000} />
-              <ProductCart name={'adidas'} url={'/products/g1.png'} price={1000} />
-              <ProductCart name={'pumer'} url={'/products/h1.png'} price={600} />
-              <ProductCart name={'pumer'} url={'/products/i1.png'} price={550} />
-              <ProductCart name={'pumer'} url={'/products/j1.png'} price={950} />
-          </div>
+                <h1 className="font-bold text-slate-700 text-2xl"> FEATURED PRODUCTS </h1>
+                <p className="text-slate-500">The most prominent product in the store, which was bought with the highest number </p>
+            </header>
+            <div className="sm:w-full overflow-hidden ">
+                <Swiper className='h-full'
+                    modules={[Navigation, Pagination]}
+                    spaceBetween={20}
+                    slidesPerView={2}
+                    navigation
+                    breakpoints={{
+                    640: { slidesPerView: 2, },
+                    768: { slidesPerView: 2, },
+                    1024: { slidesPerView: 3, },
+                    1280: { slidesPerView: 4, },
+                    1536: { slidesPerView: 5, },
+                    11920: { slidesPerView: 6, },
+
+                    }}
+                    >
+                    <SwiperSlide>
+                    <ProductCart name={'air'} url={'/products/a1.png'} price={300} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                    <ProductCart name={'nike'} url={'/products/b1.png'} price={200} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                    <ProductCart name={'adidas'} url={'/products/c1.png'} price={2500} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                    <ProductCart name={'pumer'} url={'/products/d1.png'} price={500} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                    <ProductCart name={'air'} url={'/products/e1.png'} price={700} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                    <ProductCart name={'nike'} url={'/products/f1.png'} price={1000} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                    <ProductCart name={'adidas'} url={'/products/g1.png'} price={1000} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                    <ProductCart name={'pumer'} url={'/products/h1.png'} price={600} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                    <ProductCart name={'pumer'} url={'/products/i1.png'} price={550} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                    <ProductCart name={'pumer'} url={'/products/j1.png'} price={950} />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
       </section>
   )
 }
