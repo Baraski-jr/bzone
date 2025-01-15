d7ce5abe5ec1578e0be4ab399d2a538e08fdf1c6
Omar Barra <78673626+Baraski-jr@users.noreply.github.com>
2025-01-15 02:09:14 +0000

    Inter change the FreeReview with the Promotion Component

--- app/page.tsx
+++ app/page.tsx
@@ -35,9 +35,8 @@ export default async function Home () {
         bgUrl='/kid-shoes-bg.jpeg' 
         category={"kid"}
       />
-      <FreeReview />
       <Promotion />
-
+      <FreeReview />
       <FooterComponent />
     </section>
   );
