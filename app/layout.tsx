fd362a527560f442734dd88e70fc466a464bbcc2
Omar Barra <78673626+Baraski-jr@users.noreply.github.com>
2025-01-25 13:11:07 +0000

    Remove the supabase and setup the wixCient

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { CartProvider } from "@/context/CartContext"
+import { WixClientContextProvider } from "@/context/wixContext"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <CartProvider>{children}</CartProvider>
+        <WixClientContextProvider>
+          <CartProvider>{children}</CartProvider>
+        </WixClientContextProvider>
       </body>
     </html>
   )
