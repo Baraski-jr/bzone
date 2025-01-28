87b629abcd94d095d11e76133f99e2c53b8a4380
Omar Barra <78673626+Baraski-jr@users.noreply.github.com>
2025-01-28 04:03:10 +0000

    Make the filter component reponsive

--- components/Filter/index.tsx
+++ components/Filter/index.tsx
@@ -18,48 +18,52 @@ const Filter = () => {
   }
 
   return (
-    <div className="mt-12 flex justify-between  w-[85%] mx-auto">
+    <div className="mt-12 flex flex-col gap-6 md:flex-row justify-between w-[85%] mx-auto">
       <div className="flex gap-6 flex-wrap">
-        <input
-          type="text"
-          name="min"
-          placeholder="min price"
-          className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-400"
-          onChange={handleFilterChange}
-        />
-        <input
-          type="text"
-          name="max"
-          placeholder="max price"
-          className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-400"
-          onChange={handleFilterChange}
-        />
-        {/* TODO: Filter Categories */}
-        <select
-          name="category"
-          className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
-          onChange={handleFilterChange}
-        >
-          <option>Category</option>
-          <option value="men">Men</option>
-          <option value="women">Women</option>
-          <option value="kid">Kid</option>
-        </select>
-        <select
-          name=""
-          id=""
-          title="All Filters"
-          className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
-          aria-label="All Filters"
-        >
-          <option>All Filters</option>
-        </select>
+        <div className="flex gap-x-6">
+          <input
+            type="text"
+            name="min"
+            placeholder="min price"
+            className="text-xs rounded-sm pl-2 py-1 w-24 ring-1 ring-gray-400"
+            onChange={handleFilterChange}
+          />
+          <input
+            type="text"
+            name="max"
+            placeholder="max price"
+            className="text-xs rounded-sm pl-2 py-1 w-24 ring-1 ring-gray-400"
+            onChange={handleFilterChange}
+          />
+        </div>
+        <div className="flex gap-x-6">
+          {/* TODO: Filter Categories */}
+          <select
+            name="category"
+            className="py-2 px-4 rounded-sm text-xs font-medium bg-[#EBEDED]"
+            onChange={handleFilterChange}
+          >
+            <option value="all-products">all-products</option>
+            <option value="men">Men</option>
+            <option value="women">Women</option>
+            <option value="kid">Kid</option>
+          </select>
+          <select
+            name=""
+            id=""
+            title="All Filters"
+            className="py-2 px-4 rounded-sm text-xs font-medium bg-[#EBEDED]"
+            aria-label="All Filters"
+          >
+            <option>All Filters</option>
+          </select>
+        </div>
       </div>
       <div className="">
         <select
           name="sort"
           id=""
-          className="py-2 px-4 rounded-2xl text-xs font-medium bg-white ring-1 ring-gray-400"
+          className="py-2 px-4 rounded-sm text-xs font-medium bg-white ring-1 ring-gray-400"
           onChange={handleFilterChange}
         >
           <option>Sort By</option>
