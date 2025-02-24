c47baea4bdcb0a11741bb664734f091b257ffffb
Omar Barra <78673626+Baraski-jr@users.noreply.github.com>
2025-02-24 12:03:52 +0000

    Update return_url in CheckoutForm to use relative path for payment success

--- components/CheckoutForm/index.tsx
+++ components/CheckoutForm/index.tsx
@@ -51,7 +51,7 @@ const CheckoutForm = ({ amount }: { amount: number }) => {
       elements,
       clientSecret,
       confirmParams: {
-        return_url: `http://www.localhost:3000/payment-success?amount=${amount}`,
+        return_url: `/payment-success?amount=${amount}`,
       },
     })
 
